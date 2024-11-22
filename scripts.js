1ebf51e270c57721a6ce7df76d8ed2f98ad0a9b1
sheethal1111 <sheethalshvvfgcbca@gmail.com>
2024-11-22 14:24:46 +0530

    completed

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Function to add a "scrolled" class to navbar when the page is scrolled
+window.onscroll = function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+};
+
+// Optional: You can also add smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
